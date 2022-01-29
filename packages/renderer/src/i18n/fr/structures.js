2beb780aaa16a01f00e2e1491acf09500fed02b4
Youta Obby Sidane <dyosby237@gmail.com>
2022-01-29 13:22:11 +0100

    Add Structures Lang resources

---
+++ packages/renderer/src/i18n/fr/structures.js
@@ -0,0 +1,60 @@
+export default {
+  title: 'Gestion des Structures',
+  country: {
+    listTitle: 'Liste des pays',
+    formCreateTitle: 'Ajouter un pays',
+    formUpdateTitle: 'Modifier le pays',
+    store:
+      "L'enregistrement du type de pays a ete effectue avec succes",
+    update:
+      'La modification du type de pays a ete effectuee avec succes',
+  },
+  region: {
+    listTitle: 'Liste des regions',
+    formCreateTitle: 'Ajouter une region',
+    formUpdateTitle: 'Modifier la region',
+    store: "L'enregistrement de la region a ete effectue avec succes",
+    update: "La modification de la region a ete effectuee avec succes",
+  },
+  localization: {
+    listTitle: 'Liste des adresses',
+    formCreateTitle: 'Ajouter une adresse',
+    formUpdateTitle: "Modifier l'adresse",
+    store: "L'enregistrement de l' adresse a ete effectue avec succes",
+    update: "La modification de l'adresse a ete effectuee avec succes",
+  },
+  module: {
+    listTitle: 'Liste des modules',
+    formCreateTitle: "Ajouter un module",
+    formUpdateTitle: "Modifier le module",
+    store:
+      "L'enregistrement du module a ete effectue avec succes",
+    update:
+      "La modification du module a ete effectuee avec succes",
+  },
+  enterprise: {
+    listTitle: 'Liste des entreprises',
+    formCreateTitle: "Ajouter une entreprise",
+    formUpdateTitle: "Modifier l'entreprise",
+    store:
+      "L'enregistrement de l'entreprise a ete effectue avec succes",
+    update:
+      "La modification de l'entreprise a ete effectuee avec succes",
+  },
+  enterpriseModule: {
+    listTitle: "Liste des modules de l'entreprise",
+    formCreateTitle: "Ajouter les modules a l'entreprise",
+    formUpdateTitle: "Modifier les modules a l'entreprise",
+    store:
+      "L'enregistrement des modules a l'entreprise a ete effectue avec succes",
+    update:
+      "La modification des modules a l'entreprise a ete effectuee avec succes",
+  },
+  enterpriseType: {
+    listTitle: "Liste des types d'entreprise",
+    formCreateTitle: "Ajouter un type d'entreprise",
+    formUpdateTitle: "Modifier le type d'entreprise",
+    store: "L'enregistrement du type d'entreprise a ete effectue avec succes",
+    update: "La modification du type d'entreprise a ete effectuee avec succes",
+  },
+};
