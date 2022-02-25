bb5c1a98df7f79934114fad2a8c0022c73ac6b73
Youta Obby Sidane <dyosby237@gmail.com>
2022-02-25 14:58:42 +0100

    Fix app version end

--- .electron-builder.config.js
+++ .electron-builder.config.js
@@ -1,7 +1,3 @@
-if (process.env.VITE_APP_VERSION === undefined) {
-  const now = new Date;
-  process.env.VITE_APP_VERSION = `${now.getUTCFullYear() - 2000}.${now.getUTCMonth() + 1}.${now.getUTCDate()}-${now.getUTCHours() * 60 + now.getUTCMinutes()}`;
-}
 
 /**
  * @type {import('electron-builder').Configuration}
@@ -16,7 +12,7 @@ const config = {
     'packages/**/dist/**',
   ],
   extraMetadata: {
-    version: process.env.VITE_APP_VERSION,
+    version: require('electron').version,
   },
 };
 
