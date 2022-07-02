26331feca8f6b0e0a59cc7059d9b56575b0f54e8
Tchankeu Tankeu Rodolphe <tcharod21@gmail.com>
2022-07-03 00:00:26 +0100

    remove truncate on utils

--- packages/renderer/src/helpers/utils.js
+++ packages/renderer/src/helpers/utils.js
@@ -7,7 +7,7 @@
 // }
 
 import { actionCode } from '/@/helpers/codes.js';
-import { sumBy, truncate } from 'lodash';
+import { sumBy } from 'lodash';
 // import i18n from '/@/i18n';
 import localStore from '/@/store/helpers/localStore.js';
 
