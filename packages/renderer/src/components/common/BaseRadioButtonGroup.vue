1727565e2d802d5aa8b1d16bc9870e26418e95d7
black-code1 <legrandmahamat1@gmail.com>
2022-08-08 12:30:59 +0100

    Created BaseRadioButtonGroup common component

---
+++ packages/renderer/src/components/common/BaseRadioButtonGroup.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="form-check-inline">
+    <label
+      :for="`inline-${option[keyValue]}`"
+      v-for="(option, index) in options"
+      :key="index"
+      class="form-check-label m-l-5"
+    >
+      <input
+        :id="`inline-${option[keyValue]}`"
+        type="radio"
+        name="radio-input"
+        class="form-check-input"
+        :value="option[keyValue]"
+        @change="$emit('update:modelValue', option[keyValue])"
+      />
+      {{ option[keyLabel] }}
+    </label>
+  </div>
+
+  <div v-if="errors" class="invalid-feedback" style="display: inline">
+    {{ errors[0] }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+    labelClass: {
+      type: String,
+      default: 'form-label fw-bold',
+    },
+    modelValue: {
+      type: [Number, String],
+      default: null,
+    },
+    options: {
+      type: Array,
+      required: true,
+    },
+    keyLabel: {
+      type: String,
+      default: null,
+    },
+    keyValue: {
+      type: String,
+      default: null,
+    },
+    required: {
+      type: [Boolean],
+      default: false,
+    },
+    errors: {
+      type: [Array, Object],
+      default: null,
+    },
+  },
+  emits: ['update:modelValue'],
+};
+</script>
