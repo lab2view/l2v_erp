10becd64888c5cd3ebe02feb7c5233cbf780aaa1
Tchankeu Tankeu Rodolphe <tcharod21@gmail.com>
2022-09-27 20:12:00 +0100

    Update reset password form

--- packages/renderer/src/views/auth/ForgetPassword.vue
+++ packages/renderer/src/views/auth/ForgetPassword.vue
@@ -9,30 +9,13 @@
               <div class="login-main">
                 <form
                   class="theme-form login-form"
-                  @submit.prevent="submitPhoneInput"
+                  @submit.prevent="
+                    haveValidOtp ? handleResetPassword : handleResetPasswordForm
+                  "
+                  v-if="!haveValidOtp"
                 >
                   <h4 class="mb-3">Reset Your Password</h4>
-                  <div class="form-group">
-                    <label>Enter Your Mobile Number</label>
-                    <div class="row">
-                      <div class="col-8 col-sm-9">
-                        <BaseInput
-                          v-model="inputField.phone"
-                          type="text"
-                          placeholder="000-000-000"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  <div class="form-group">
-                    <BaseButton
-                      type="submit"
-                      class="btn btn-primary btn-block"
-                      :text="$t('common.send')"
-                    />
-                  </div>
-
-                  <div v-if="requestId" class="form-group">
+                  <div v-if="isOtpField" class="form-group">
                     <span class="reset-password-link">
                       If don't receive OTP ?
                       <a
@@ -44,18 +27,29 @@
                       </a>
                     </span>
                   </div>
-
-                  <div v-if="requestId" class="form-group">
+                  <div v-if="!isOtpField" class="form-group">
+                    <label>Enter Your Mobile Number</label>
+                    <div class="row">
+                      <div class="col-8 col-sm-9">
+                        <BaseInput
+                          v-model.number="inputField.phone"
+                          type="number"
+                          placeholder="000-000-000"
+                          :errors="errors?.phone"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div v-else class="form-group">
                     <label>Enter OTP</label>
                     <div class="row">
                       <div class="col-8 col-sm-9">
                         <BaseInput
                           v-model="inputField.otp_code"
                           class="form-control opt-text"
-                          type="text"
+                          :errors="errors?.otp_code"
                           placeholder="000000"
                           maxlength="6"
-                          @click.prevent="submitOtp"
                         />
                       </div>
                     </div>
@@ -67,59 +61,66 @@
                       />
                     </div>
                   </div>
-                </form>
 
-                <form
-                  v-if="resetPasswordToken"
-                  class="theme-form login-form"
-                  @submit.prevent="resetPassword"
-                >
-                  <h6>Create Your Password</h6>
                   <div class="form-group">
-                    <label>New Password</label>
-                    <div class="input-group">
-                      <span class="input-group-text"
-                      ><i class="icon-lock"></i
-                      ></span>
-                      <BaseInput
-                        v-model="inputField.password"
-                        class="form-control"
-                        type="password"
-                        name="login[password]"
-                        required=""
-                        placeholder="*********"
-                      />
-                      <div class="show-hide"><span class="show"></span></div>
+                    <BaseButton
+                      type="submit"
+                      class="btn btn-primary btn-block"
+                      :text="
+                        isOtpField ? $t('common.verify') : $t('common.send')
+                      "
+                    />
+                  </div>
+
+                  <div v-if="haveValidOtp">
+                    <h6>Create Your Password</h6>
+                    <div class="form-group">
+                      <label>New Password</label>
+                      <div class="input-group">
+                        <span class="input-group-text"
+                          ><i class="icon-lock"></i
+                        ></span>
+                        <BaseInput
+                          v-model="inputField.password"
+                          class="form-control"
+                          type="password"
+                          name="login[password]"
+                          required
+                          placeholder="*********"
+                        />
+                        <div class="show-hide"><span class="show"></span></div>
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <label>Retype Password</label>
+                      <div class="input-group">
+                        <span class="input-group-text"
+                          ><i class="icon-lock"></i
+                        ></span>
+                        <BaseInput
+                          v-model="inputField.password_confirmation"
+                          class="form-control"
+                          type="password"
+                          name="login[password]"
+                          required
+                          placeholder="*********"
+                        />
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <button class="btn btn-primary btn-block" type="submit">
+                        Done
+                      </button>
                     </div>
                   </div>
-                  <div class="form-group">
-                    <label>Retype Password</label>
-                    <div class="input-group">
-                      <span class="input-group-text"
-                      ><i class="icon-lock"></i
-                      ></span>
-                      <BaseInput
-                        v-model="inputField.password_confirmation"
-                        class="form-control"
-                        type="password"
-                        name="login[password]"
-                        required=""
-                        placeholder="*********"
-                      />
-                    </div>
-                  </div>
-                  <div class="form-group">
-                    <button class="btn btn-primary btn-block" type="submit">
-                      Done
-                    </button>
-                  </div>
+
                   <p>
                     Already have an password?<a
-                    class="ms-2"
-                    href="#"
-                    @click.prevent="$router.push({ name: 'login' })"
-                  >Sign in</a
-                  >
+                      class="ms-2"
+                      href="#"
+                      @click.prevent="$router.push({ name: 'login' })"
+                      >Sign in</a
+                    >
                   </p>
                 </form>
               </div>
@@ -135,57 +136,59 @@
 <script>
 import BaseInput from '/@/components/common/BaseInput.vue';
 import BaseButton from '/@/components/common/BaseButton.vue';
-import {mapGetters, mapState} from 'vuex';
+import { mapGetters, mapState } from 'vuex';
 
 export default {
   name: 'ForgetPassword',
-  components: {BaseButton, BaseInput},
+  components: { BaseButton, BaseInput },
 
   data() {
     return {
       loading: false,
       errors: [],
       inputField: {
-        phone: '',
-        otp_code: '',
-        password: '',
-        password_confirmation: '',
+        phone: null,
+        otp_code: null,
+        password: null,
+        password_confirmation: null,
       },
     };
   },
   computed: {
     ...mapGetters('auth', ['requestId']),
     ...mapState('auth', ['resetPasswordToken']),
-
-    handlePhoneForm() {
-      return this.requestId
-    }
+    haveValidOtp() {
+      return !!this.resetPasswordToken;
+    },
+    isOtpField() {
+      return !!this.requestId;
+    },
   },
   methods: {
-    submitPhoneInput() {
-      this.$store
-        .dispatch('auth/sendResetPasswordCode', this.inputField.phone)
-        .catch((error) => {
-          this.errors = error.response?.data?.errors;
-          console.log(error);
-          this.loading = false;
-        });
+    handleResetPasswordForm() {
+      if (!this.isOtpField) {
+        return this.$store
+          .dispatch('auth/sendResetPasswordCode', '' + this.inputField.phone)
+          .catch((error) => {
+            this.errors = error.response?.data?.errors;
+            console.log(error);
+          })
+          .finally(() => (this.loading = false));
+      } else {
+        return this.$store
+          .dispatch('auth/verifyOtpCode', {
+            code: this.inputField.otp_code,
+            request_id: this.requestId,
+          })
+          .catch((error) => {
+            this.errors = error.response?.data?.errors;
+            console.log(error);
+          })
+          .finally(() => (this.loading = false));
+      }
     },
 
-    submitOtp() {
-      this.$store
-        .dispatch('auth/verifyOtpCode', {
-          code: this.inputField.otp_code,
-          request_id: this.requestId,
-        })
-        .catch((error) => {
-          this.errors = error.response?.data?.errors;
-          console.log(error);
-          this.loading = false;
-        });
-    },
-
-    resetPassword() {
+    handleResetPassword() {
       this.$store
         .dispatch('auth/resetPassword', {
           password: this.inputField.password,
