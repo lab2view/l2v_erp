1d1a77d89c888e5a851a3c79093099f6842e3511
Tchankeu Tankeu Rodolphe <tcharod21@gmail.com>
2022-10-06 19:36:19 +0100

    remake inventory form structure

---
+++ packages/renderer/src/views/stocks/inventories/forms/InventoryLineList.vue
@@ -0,0 +1,190 @@
+<template>
+  <router-view v-if="!inventoryIsConfirm" />
+  <div v-if="$route.name === 'stocks.entry.form.article'" class="card mb-0">
+    <div class="card-header pb-0">
+      <div class="row align-items-center">
+        <div class="col-sm">
+          <h5>
+            {{ `${$t('stocks.entryLine.list')} - ${inventoryReference}` }}
+          </h5>
+        </div>
+        <div v-if="!inventoryIsConfirm" class="col-sm-auto align-items-end">
+          <BaseButton
+            type="button"
+            class="btn btn-outline-danger m-r-5"
+            :disabled="!isSelected"
+            icon="fa fa-trash-o"
+            :text="$t('common.delete_all')"
+            :loading="loading"
+            @click.prevent="deleteSelectedInventoryLine"
+          />
+          <router-link
+            :to="{
+              name: 'stocks.entry.form.article.form',
+              params: $route.params,
+              query: $route.query,
+            }"
+            class="btn btn-primary"
+            type="button"
+          >
+            <i class="fa fa-plus m-r-5" />
+            {{ $t('common.add_article') }}
+          </router-link>
+        </div>
+      </div>
+    </div>
+    <div class="card-body">
+      <div class="table-responsive">
+        <table class="table">
+          <thead>
+            <tr>
+              <th :title="$t('common.select_all')">
+                <div
+                  :class="`${
+                    partialSelect
+                      ? 'checkbox-solid-success'
+                      : 'checkbox-primary'
+                  }
+                      ${!inventoryIsConfirm ? 'checkbox' : ''}
+                  `"
+                >
+                  <input
+                    v-if="!inventoryIsConfirm"
+                    id="checkbox-stock-line-1"
+                    v-model="selectAll"
+                    type="checkbox"
+                  />
+                  <label
+                    class="m-0 pt-0 pb-0 p-l-5"
+                    for="checkbox-stock-line-1"
+                    :style="{ 'padding-left: 60px': !inventoryIsConfirm }"
+                  >
+                    {{ `${$t('articles.listTitle')} ${countSelected}` }}</label
+                  >
+                </div>
+              </th>
+              <th class="text-center" scope="col">
+                {{ $t('common.attributes.quantity') }}
+              </th>
+              <th class="text-center" scope="col">
+                {{ $t('common.attributes.buying_price') }}
+              </th>
+              <th v-if="!inventoryIsConfirm" scope="col">
+                {{ $t('common.actions') }}
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <ArticleLineSelectable
+              v-for="(inventoryLine, index) in inventoryLines"
+              :key="`sck-ent-lne-${index}`"
+              :model="inventoryLine"
+              :selected-list="selected"
+              update-dispatch-name="stock_entry/updateInventoryLine"
+              remove-dispatch-name="stock_entry/removeInventoryLines"
+              :cancel-selection="inventoryIsConfirm"
+              @deleted="selected = []"
+              @selected="selectInventoryLine(inventoryLine, true)"
+              @unselected="selectInventoryLine(inventoryLine, false)"
+            >
+              <td class="text-center">{{ inventoryLine.buying_price }}</td>
+            </ArticleLineSelectable>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <div v-if="inventoryIsConfirm" class="card-footer border-top-0">
+      <div class="row justify-content-center">
+        <BaseButton
+          class="btn btn-success col-auto"
+          type="button"
+          :text="$t('common.confirm_provision')"
+          icon="fa fa-check-circle"
+          :loading="loading"
+          @click.prevent=""
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import BaseButton from '/@/components/common/BaseButton.vue';
+import ArticleLineSelectable from '/@/components/articles/ArticleLineSelectable.vue';
+
+export default {
+  name: 'InventoryLineList',
+  components: { ArticleLineSelectable, BaseButton },
+  data() {
+    return {
+      selected: [],
+      loading: false,
+    };
+  },
+  computed: {
+    ...mapGetters('inventory', [
+      'inventoryLines',
+      'inventoryReference',
+      'inventoryIsConfirm',
+    ]),
+    partialSelect() {
+      return (
+        this.selected.length > 0 &&
+        this.selected.length < this.inventoryLines.length
+      );
+    },
+    selectedAllInventoryLine() {
+      if (this.inventoryLines.length)
+        return this.selected.length === this.inventoryLines.length;
+      else return false;
+    },
+    selectAll: {
+      get() {
+        return this.selectedAllInventoryLine;
+      },
+      set(value) {
+        if (!value) this.selected = [];
+        else {
+          let result = [];
+          this.inventoryLines.forEach((ag) => result.push(ag.id));
+          this.selected = result;
+        }
+      },
+    },
+    isSelected() {
+      return this.selected.length > 0;
+    },
+    countSelected() {
+      return this.isSelected ? `( ${this.selected.length} )` : '';
+    },
+  },
+  methods: {
+    selectInventoryLine(inventoryLine, adding) {
+      if (adding) this.selected.push(inventoryLine.id);
+      else
+        this.selected = this.selected.filter((id) => id !== inventoryLine.id);
+    },
+    deleteSelectedInventoryLine() {
+      if (
+        this.selected.length &&
+        confirm(
+          this.$t('messages.confirmDelete', {
+            label: this.$t('common.deleted_selection'),
+          })
+        )
+      ) {
+        this.loading = true;
+        this.$store
+          .dispatch('inventory/removeInventoryLines', this.selected)
+          .then(() => {
+            this.loading = false;
+            this.selected = [];
+          });
+      }
+    },
+  },
+};
+</script>
+
+<style scoped></style>
