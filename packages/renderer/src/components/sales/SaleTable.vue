f7ccc0225db3924e917b62020939ec4d9cc57cfa
black-code1 <legrandmahamat1@gmail.com>
2023-03-27 13:40:05 +0100

    Add vertical scroll on the sales of the cash reports

--- packages/renderer/src/components/sales/SaleTable.vue
+++ packages/renderer/src/components/sales/SaleTable.vue
@@ -1,118 +1,176 @@
 <template>
-  <BaseDatatable
-    v-if="!$store.state.globalLoading"
-    :tfoot="true"
-    :total="sales.length"
-    :scroll-y="isCashierSession ? '530px' : false"
-  >
-    <template #headers>
-      <th>#</th>
-      <th v-if="isRoleAdmin">{{ $t('common.attributes.structure') }}</th>
-      <th>{{ $t('common.attributes.reference') }}</th>
-      <th>{{ $t('common.headers.quantity') }}</th>
-      <th>{{ $t('common.headers.sub_amount') }}</th>
-      <th>{{ $t('common.attributes.discount') }}</th>
-      <th>{{ $t('common.headers.total_price') }}</th>
-      <th v-if="showTotalWinAmount">{{ $t('common.headers.win_amount') }}</th>
-      <th>
-        {{ $t(`common.attributes.${isCashierSession ? 'time' : 'date'}`) }}
-      </th>
-      <th v-if="!isCashierSession">{{ $t('common.actions') }}</th>
-    </template>
-    <tr v-for="sale in sales" :key="`sale-line-${sale.id}`">
-      <td>{{ sale.id }}</td>
-      <td v-if="isRoleAdmin">
-        {{ sale.enterprise.name ?? $t('common.parent') }}
-      </td>
-      <td>
-        <router-link
-          v-if="isCashierSession"
-          :to="{
+  <div>
+    <BaseDatatable
+      v-if="!$store.state.globalLoading"
+      :tfoot="true"
+      :total="sales.length"
+      :scroll-x="isCashierSession ? '530px' : false"
+      :scroll-y="isCashierSession ?  '110px' : false"
+    >
+      <template #headers>
+        <th>#</th>
+        <th v-if="isRoleAdmin">{{ $t('common.attributes.structure') }}</th>
+        <th>{{ $t('common.attributes.reference') }}</th>
+        <th>{{ $t('common.headers.quantity') }}</th>
+        <th>{{ $t('common.headers.sub_amount') }}</th>
+        <th>{{ $t('common.attributes.discount') }}</th>
+        <th>{{ $t('common.headers.total_price') }}</th>
+        <th v-if="showTotalWinAmount">{{ $t('common.headers.win_amount') }}</th>
+        <th>
+          {{ $t(`common.attributes.${isCashierSession ? 'time' : 'date'}`) }}
+        </th>
+        <th v-if="!isCashierSession">{{ $t('common.actions') }}</th>
+      </template>
+      <tr v-for="sale in sales" :key="`sale-line-${sale.id}`">
+        <td>{{ sale.id }}</td>
+        <td v-if="isRoleAdmin">
+          {{ sale.enterprise.name ?? $t('common.parent') }}
+        </td>
+        <td>
+          <router-link
+            v-if="isCashierSession"
+            :to="{
             name: 'sales.session.reports.details',
             params: { id: sale.id },
           }"
-        >
-          {{ sale.code }}
-        </router-link>
-        <span v-else>{{ sale.code }}</span>
-      </td>
-      <td>
-        <label
-          v-for="(quantity, index) in sale.quantities"
-          :key="`qty-${index}`"
-          class="f-w-700 m-r-5"
-        >
-          {{ quantity.total }}
-          <i class="f-w-400">{{ quantity.unit }}</i>
-        </label>
-      </td>
-      <td>{{ `${sale.sup_amount}` }}</td>
-      <td>{{ sale.discount }}</td>
-      <td>{{ sale.sale_amount }}</td>
-      <td v-if="showTotalWinAmount">
-        {{ `${sale.sale_win_amount}` }}
-        <i
-          :class="`f-w-400 font-${
+          >
+            {{ sale.code }}
+          </router-link>
+          <span v-else>{{ sale.code }}</span>
+        </td>
+        <td>
+          <label
+            v-for="(quantity, index) in sale.quantities"
+            :key="`qty-${index}`"
+            class="f-w-700 m-r-5"
+          >
+            {{ quantity.total }}
+            <i class="f-w-400">{{ quantity.unit }}</i>
+          </label>
+        </td>
+        <td>{{ `${sale.sup_amount}` }}</td>
+        <td>{{ sale.discount }}</td>
+        <td>{{ sale.sale_amount }}</td>
+        <td v-if="showTotalWinAmount">
+          {{ `${sale.sale_win_amount}` }}
+          <i
+            :class="`f-w-400 font-${
             sale.sale_win_amount > 0 ? 'primary' : 'danger'
           }`"
-        >
-          {{ `(${sale.sale_win_amount_percent}%)` }}
-        </i>
-      </td>
-      <td>{{ $d(sale.created_at, isCashierSession ? 'time' : 'short') }}</td>
-      <td v-if="!isCashierSession">
-        <BaseActionBtnGroup
-          entity="Sale"
-          :with-update-btn="false"
-          @show="
+          >
+            {{ `(${sale.sale_win_amount_percent}%)` }}
+          </i>
+        </td>
+        <td>{{ $d(sale.created_at, isCashierSession ? 'time' : 'short') }}</td>
+        <td v-if="!isCashierSession">
+          <BaseActionBtnGroup
+            entity="Sale"
+            :with-update-btn="false"
+            @show="
             $router.push({
               name: 'sales.sales.details',
               params: { id: sale.id },
             })
           "
-          @delete="deleteSale(sale)"
-        />
-      </td>
-    </tr>
-    <template #footers>
-      <th :colspan="footerTotalColspan" class="text-center">
-        {{ $t('common.headers.total').toUpperCase() }}
-      </th>
-      <th colspan="2" class="text-end">
-        <label
-          v-for="(saleTotalQuantity, index) in saleTotalQuantities"
-          :key="`sale-total-qty-${index}`"
-          class="f-w-700 m-r-5"
-        >
-          {{ saleTotalQuantity.total }}
-          <i class="f-w-400">{{ saleTotalQuantity.unit }}</i>
-        </label>
-      </th>
-      <th>{{ `${saleTotalSupAmount} ${currency}` }}</th>
-      <th>{{ `${saleTotalDiscount} ${currency}` }}</th>
-      <th :colspan="isCashierSession ? 2 : 0">
-        {{ `${saleTotalSaleAmount} ${currency}` }}
-      </th>
-      <th v-if="showTotalWinAmount" colspan="3">
-        {{ `${saleTotalWinAmount} ${currency}` }}
-        <label :class="`font-${saleTotalWinAmount > 0 ? 'primary' : 'danger'}`">
-          ({{ `${saleTotalWinAmountPercent} %` }})
-        </label>
-      </th>
-    </template>
-  </BaseDatatable>
+            @delete="deleteSale(sale)"
+          />
+        </td>
+      </tr>
+      <tr v-for="sale in sales" :key="`sale-line-${sale.id}`">
+        <td>{{ sale.id }}</td>
+        <td v-if="isRoleAdmin">
+          {{ sale.enterprise.name ?? $t('common.parent') }}
+        </td>
+        <td>
+          <router-link
+            v-if="isCashierSession"
+            :to="{
+            name: 'sales.session.reports.details',
+            params: { id: sale.id },
+          }"
+          >
+            {{ sale.code }}
+          </router-link>
+          <span v-else>{{ sale.code }}</span>
+        </td>
+        <td>
+          <label
+            v-for="(quantity, index) in sale.quantities"
+            :key="`qty-${index}`"
+            class="f-w-700 m-r-5"
+          >
+            {{ quantity.total }}
+            <i class="f-w-400">{{ quantity.unit }}</i>
+          </label>
+        </td>
+        <td>{{ `${sale.sup_amount}` }}</td>
+        <td>{{ sale.discount }}</td>
+        <td>{{ sale.sale_amount }}</td>
+        <td v-if="showTotalWinAmount">
+          {{ `${sale.sale_win_amount}` }}
+          <i
+            :class="`f-w-400 font-${
+            sale.sale_win_amount > 0 ? 'primary' : 'danger'
+          }`"
+          >
+            {{ `(${sale.sale_win_amount_percent}%)` }}
+          </i>
+        </td>
+        <td>{{ $d(sale.created_at, isCashierSession ? 'time' : 'short') }}</td>
+        <td v-if="!isCashierSession">
+          <BaseActionBtnGroup
+            entity="Sale"
+            :with-update-btn="false"
+            @show="
+            $router.push({
+              name: 'sales.sales.details',
+              params: { id: sale.id },
+            })
+          "
+            @delete="deleteSale(sale)"
+          />
+        </td>
+      </tr>
+      <template #footers>
+        <th :colspan="footerTotalColspan" class="text-center">
+          {{ $t('common.headers.total').toUpperCase() }}
+        </th>
+        <th colspan="2" class="text-end">
+          <label
+            v-for="(saleTotalQuantity, index) in saleTotalQuantities"
+            :key="`sale-total-qty-${index}`"
+            class="f-w-700 m-r-5"
+          >
+            {{ saleTotalQuantity.total }}
+            <i class="f-w-400">{{ saleTotalQuantity.unit }}</i>
+          </label>
+        </th>
+        <th>{{ `${saleTotalSupAmount} ${currency}` }}</th>
+        <th>{{ `${saleTotalDiscount} ${currency}` }}</th>
+        <th :colspan="isCashierSession ? 2 : 0">
+          {{ `${saleTotalSaleAmount} ${currency}` }}
+        </th>
+        <th v-if="showTotalWinAmount" colspan="3">
+          {{ `${saleTotalWinAmount} ${currency}` }}
+          <label :class="`font-${saleTotalWinAmount > 0 ? 'primary' : 'danger'}`">
+            ({{ `${saleTotalWinAmountPercent} %` }})
+          </label>
+        </th>
+      </template>
+    </BaseDatatable>
+  </div>
 </template>
 
 <script>
 import BaseDatatable from '/@/components/common/BaseDatatable.vue';
-import { mapGetters } from 'vuex';
-import { getSaleAmount } from '/@/helpers/utils';
+import {mapGetters} from 'vuex';
+import {getSaleAmount} from '/@/helpers/utils';
 import _ from 'lodash';
 import BaseActionBtnGroup from '/@/components/common/BaseActionBtnGroup.vue';
 
 export default {
   name: 'SaleTable',
-  components: { BaseActionBtnGroup, BaseDatatable },
+  components: {BaseActionBtnGroup, BaseDatatable},
   props: {
     sales: {
       type: Array,
@@ -147,9 +205,9 @@ export default {
     saleTotalWinAmountPercent() {
       return this.saleTotalAmountAfterDiscount > 0
         ? (
-            (this.saleTotalWinAmount * 100) /
-            this.saleTotalAmountAfterDiscount
-          ).toFixed(2)
+          (this.saleTotalWinAmount * 100) /
+          this.saleTotalAmountAfterDiscount
+        ).toFixed(2)
         : 100;
     },
     saleTotalQuantities() {
@@ -174,7 +232,7 @@ export default {
   methods: {
     deleteSale(sale) {
       if (
-        confirm(this.$t('messages.confirmDelete', { label: sale.reference }))
+        confirm(this.$t('messages.confirmDelete', {label: sale.reference}))
       ) {
         this.$store.commit('SET_GLOBAL_LOADING', true);
         this.$store
@@ -190,4 +248,10 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.table-wrapper {
+  max-height: 530px;
+  overflow: auto;
+  display: inline-block;
+}
+</style>
